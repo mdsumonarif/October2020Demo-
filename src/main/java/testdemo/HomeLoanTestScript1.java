6394cc11265d47943cd04666893fe3e7b1cfdfa4
mdsumonarif <arifur1104@gmail.com>
2021-04-21 17:16:47 -0500

    Signed-off-by: mdsumonarif <arifur1104@gmail.com>

---
+++ src/main/java/testdemo/HomeLoanTestScript1.java
@@ -0,0 +1,11 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class HomeLoanTestScript1 {
+	@Test
+	public void homeloan() {
+	System.out.println("homeloan functionality");
+
+}
+}
