d129336af5c65ca82f20d92054a786ad5c5f73b2
mdsumonarif <arifur1104@gmail.com>
2021-04-21 16:30:45 -0500

    Signed-off-by: mdsumonarif <arifur1104@gmail.com>

---
+++ src/main/java/testdemo/TestClass1.java
@@ -0,0 +1,13 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	
+	@Test
+	public void login() {
+		System.out.println("loginto functionality");
+		System.out.println("push code functionality");
+	}
+}
---
+++ src/main/java/testdemo/TestClass2.java
@@ -0,0 +1,15 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+	public class TestClass1 {
+
+		
+		@Test
+		public void login() {
+			System.out.println("logout to functionality");
+		}
+	}
+	}
